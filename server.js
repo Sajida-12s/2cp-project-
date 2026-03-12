cbdabf75ffb5f88944bc6c6b29d2fa7e5835adcc
Sajida-12s <s.sallouhi@esi-sba.dz>
2026-03-12 11:39:39 +0100

    update

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const authRoutes = require("./routes/auth");
+require("dotenv").config();
 
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
